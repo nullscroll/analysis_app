926fa0a17f5640b57e4fea4a589d9fd11006bc63
Paweł Rzońca <p.rzonca@widmo.tech>
2024-08-13 16:14:05 +0200

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, LogOutView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup")
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("logout/", LogOutView.as_view(), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic.base import TemplateView
+from django.contrib.auth import logout
+from django.http import HttpRequest
+from django.shortcuts import render
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +12,11 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class LogOutView(TemplateView):
+    template_name = "registration/logout.html"
+
+    def get(self, request: HttpRequest):
+        logout(request)
+        return render(request, self.template_name)
