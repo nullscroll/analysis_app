67128d5b41c7b408951c72b4a7206c7f0cb7562c
Paweł Rzońca <p.rzonca@widmo.tech>
2024-08-19 14:03:08 +0200

    progress

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Project(models.Model):
+    user = models.ForeignKey(
+            settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE,
+    )
+    name = models.CharField("name", max_length=200)
+    create_date = models.DateTimeField("create date")
+    last_edit_date = models.DateTimeField("last edit date")
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    event = models.CharField("event", max_length=100)
+    name = models.CharField("name", max_length=100)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.event}/{self.name}"
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Measurement
 # Create your views here.
+
+def index(request):
+    projects_list = Project.objects.all()
+    context = {"projects_list":projects_list}
+    return render(request, "projects/index.html", context)
+
+def details(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    m = project.measurement_set.all()
+    return render(request, "projects/details.html", {"measurement_list":m})
+
